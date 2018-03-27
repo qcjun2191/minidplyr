9bd5f9996de6d2091906f32a9c6718f5b0b7f02c
QcJun <ngotuandung1991@gmail.com>
2018-03-27 10:30:16 +0200

    Fix check

--- R/Filter1.R
+++ R/Filter1.R
@@ -1,12 +1,12 @@
 #' Title
 #'
-#' @param x 
-#' @param ind 
+#' @param x asds
+#' @param ind asda
 #'
-#' @return
+#' @return jhdhjsd
 #' @export
 #'
-#' @examples
+#' @examples filter2(iris,1)
 filter2 <- function(x,ind){
   x[ind,]
 }
---
+++ R/Select1.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x asds
+#' @param vars asd
+#'
+#' @return asdsd
+#' @export
+#'
+#' @examples select2(iris,1)
+select2 <- function(x,vars) {
+  x[vars]
+}
